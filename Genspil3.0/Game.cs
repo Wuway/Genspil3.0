6a161b8176eb51be9a3350041e03d0578c9d90dc
Wuway <contactdieter@gmail.com>
2025-04-08 21:13:46 +0200

    Fjernet lambda's L166 + L174 forkortet {get; set;} L16-L28

--- Genspil3.0/Game.cs
+++ Genspil3.0/Game.cs
@@ -2,7 +2,7 @@
 {
     class Game
     {
-        //TODO: Sæt variablene på samme linje
+        
         private string titleGame;
         private string versionGame;
         private string genreGame;
@@ -13,38 +13,20 @@
         private int amountGame;
 
         //TODO: Tilføj nogle betingelser
-        public string Title//TODO: eksempel betingelse: ToUpper (se searchshowgameresults()
-        {
-            get { return titleGame; }
-            set { titleGame = value; }
-        }
-        public string Version
-        {
-            get { return versionGame; }
-            set { versionGame = value; }
-        }
-        public string Genre//TODO: eksempel betingelse: ToUpper (se searchshowgameresults()
-        {
-            get { return genreGame; }
-            set { genreGame = value; }
-        }
-        public int ParticipantGame
-        {
-            get { return participantGame; }
-            set { participantGame = value; }
-        }
-        public int AgePlayerGame
-        {
-            get { return agePlayerGame; }
-            set { agePlayerGame = value; }
-        }
-        //TODO: lave ConditionGame om til ENUM
+        public string Title { get; set; }//TODO: eksempel betingelse: ToUpper (se searchshowgameresults()
+        
+        public string Version { get; set; }
+
+        public string Genre { get; set; }//TODO: eksempel betingelse: ToUpper (se searchshowgameresults()
+
+        public int ParticipantGame { get; set; }
+
+        public int AgePlayerGame { get; set; }
+
+        
         //OBS: Jeg har tilføjet en enum nedenunder, men resten af koden skal tilpasses! Kennie
-        public ConditionOfGame Condition
-        {
-            get { return conditionOfGame; }
-            set { conditionOfGame = value; }
-        }
+        public ConditionOfGame Condition { get; set; }
+
         //Enum til at definere betingelserne for spillet
         public enum ConditionOfGame
         {
@@ -91,8 +73,8 @@
             games.Add(new Game { titleGame = "Ticket To Ride", versionGame = "Junior", genreGame = "Børn", participantGame = 4, agePlayerGame = 6, Condition = ConditionOfGame.Fremragende, priceGame = 109, amountGame = 1 });
             SaveGamesToFile();
         }
-        //Constructor brugt til at intitialisere predefinerede data ovenover
-        public Game() { }//TODO: flyt til linje 70 (før constructor)
+        
+        public Game() { }
 
         //Constructor til at initialisere user definerede game objekter
         public Game(string titleGame, string versionGame, string genreGame, int participantGame, int agePlayerGame, ConditionOfGame conditionGame , double priceGame, int amountGame)
@@ -121,14 +103,14 @@
             Console.WriteLine("Spillets aldersgrænse (min. alder): ");
             int agePlayerGame = int.Parse(Console.ReadLine());
             Console.WriteLine("Spillets stand nyt (3), god men brugt (2), slidt (1) og reperation (0): ");
-           Enum.TryParse(Console.ReadLine(), out ConditionOfGame conditionGame);//TODO: Hvis implementering af enum i game klasse, tilpas her.
+           Enum.TryParse(Console.ReadLine(), out ConditionOfGame conditionGame);
             Console.WriteLine("Spillets pris: ");
             double priceGame = double.Parse(Console.ReadLine());
             Console.WriteLine("Tilføj antal: ");
             int amountGame = int.Parse(Console.ReadLine());
             Console.WriteLine("Vil du gemme ? (Ja / Nej)");
             string saveGame = Console.ReadLine();
-            string upperSaveGame = saveGame.ToUpper();//TODO: flyt To.Upper() metoden sammen med Console.Readline(), fjern upperSaveGame. erstat upperSaveGame i if parameter nedunder med saveGame.
+            string upperSaveGame = saveGame.ToUpper();
 
             if (upperSaveGame == "JA")
             {
@@ -163,25 +145,43 @@
             Console.Clear();
             Console.WriteLine("Du har valgt, at du vil slette et spil. Vælg mellem følgende:\n\n");
             Console.WriteLine("Lagerliste sorteret efter titel:\n--------------------------------");
-            var sortedGenre = games.OrderBy(g => g.genreGame).ToList();//TODO: find alternativer til lambda's
-            foreach (var g in sortedGenre)//TODO: valg mere beskrivende end "g" f.eks "item"
+
+            //var sortedGenre = games.OrderBy(g => g.genreGame).ToList();//TODO: find alternativer til lambda's
+            //UDEN LAMBDA VIRKER MEN LISTEN ER IKKE SORTERET
+            //MED LAMBDA ER LISTEN HELLER IKKE SORTERET
+            
+            foreach (var g in games)
             {
                 Console.WriteLine($"Spil: {g.titleGame}\nUdgave: {g.versionGame}\nGenre: {g.genreGame}\nMax antal spillere: {g.participantGame}\nMin. aldersgrænse: {g.agePlayerGame}\nStand: {g.conditionOfGame}\nPris: {g.priceGame}\nAntal: {g.amountGame}");
                 Console.WriteLine("-----------------------------------\n");
             }
             Console.WriteLine("Indtast spillets navn du ønsker slettet: ");
-            string deleteGame = Console.ReadLine().ToUpper();//TODO: give Kristina klap på skulderen
-            var game = games.FirstOrDefault(g => g.titleGame.ToUpper() == deleteGame);//TODO: Find anden metode 
-            if (game != null)
+            string deleteGame = Console.ReadLine().ToUpper();
+            //var game = games.FirstOrDefault(g => g.titleGame.ToUpper() == deleteGame);//TODO: Find anden metode 
+
+            //initialiser en variable foundGame af typen Game med værdi null.
+            //Hvorfor null? -> indikerer ingen spil er fundet endnu. Bruges senere til at tjekke om foundGame stadig er null eller om den har fået et game objekt.
+            Game foundGame = null;
+            //
+            foreach (var g in games)
+            {
+                if (g.titleGame.ToUpper() == deleteGame)
+                {
+                    foundGame = g; //værdien af g bliver "assigned" til foundGame
+                    break;
+                }
+            }
+            //har byttet game med foundGame
+            if (foundGame != null)
             {
                 Console.WriteLine("Indtast det antal du ønsker slettet: ");
-                int deleteAmount = int.Parse(Console.ReadLine());//TODO: TryParse?
-                if (game.amountGame >= deleteAmount)
+                int deleteAmount = int.Parse(Console.ReadLine());
+                if (foundGame.amountGame >= deleteAmount)
                 {
-                    game.amountGame -= deleteAmount;
-                    if (game.amountGame == 0)
+                    foundGame.amountGame -= deleteAmount;
+                    if (foundGame.amountGame == 0)
                     {
-                        games.Remove(game);//Kalder metoden Remove med game paramter på listobjekten games til at fjerne et bestemt spil.
+                        games.Remove(foundGame);//Kalder metoden Remove med game paramter på listobjekten games til at fjerne et bestemt spil.
                         Console.Clear();
                         Console.WriteLine("Spillet er slettet fra lagerlisten.\n\nIndtast vilkårlig tast for at blive sendt til hovedmenuen.\n");
                         Console.ReadLine();
@@ -190,7 +190,7 @@
                     else
                     {
                         Console.Clear();
-                        Console.WriteLine($"Spillet er slettet. Antallet af spil '{deleteGame}' er reduceret med {deleteAmount}.\n\nOpdateret lagerstatus for det valgte spil er nu: {game.amountGame}.\n\nIndtast vilkårlig tast for at blive sendt til hovedmenuen.\n");
+                        Console.WriteLine($"Spillet er slettet. Antallet af spil '{deleteGame}' er reduceret med {deleteAmount}.\n\nOpdateret lagerstatus for det valgte spil er nu: {foundGame.amountGame}.\n\nIndtast vilkårlig tast for at blive sendt til hovedmenuen.\n");
                         Console.ReadLine();
                         return;
                     }
@@ -223,5 +223,6 @@
                 games = DataHandler.LoadFromFile<Game>(filePath);
             }
         }
+
     }
 }
